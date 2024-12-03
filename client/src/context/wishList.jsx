5f02d361cae5a839946ece81dd88851efbac20bb
Kar0r404 <mahlarekarabo.702@gmail.com>
2024-12-03 16:41:12 +0200

    wishlist functionality complete

--- client/src/context/wishList.jsx
+++ client/src/context/wishList.jsx
@@ -1,29 +1,105 @@
-import { createContext, useEffect, useState } from "react";
-
+import { createContext, useContext, useEffect, useState } from "react";
+import { AuthContext } from "./authContext";
 export const WishListContext = createContext();
 
 export const WishListProvider = ({ children }) => {
   const [wishListItems, setWishListItems] = useState(
     localStorage.getItem('wishListItems') ? JSON.parse(localStorage.getItem('wishListItems')) : []
   );
+  const isAuthenticated = useContext(AuthContext);
 
-  const addToWishList = (item) => {
+
+  useEffect(() => {
+    if (isAuthenticated){
+
+        const fetchData = async () => {
+          try {
+            // Fetch the initial wishlist
+            const response = await fetch('/get-wishlist', {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+              },
+            });
+    
+            if (!response.ok) return console.error('Failed to fetch wishlist:', response.statusText);
+              const wishlist = await response.json();
+    
+              // Fetch details for each product in the wishlist
+              const productDetails = await Promise.all(
+                wishlist.map(async (item) => {
+                  const res = await fetch(`/products/${item.product_id}`, {
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                  });
+    
+                  if (res.ok) {
+                    return res.json();
+                  }
+                  return null;
+                })
+              );
+    
+              // Filter out any null responses (failed fetches) and update state
+              setWishListItems(productDetails.filter((item) => item !== null));
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+    }
+  }, [isAuthenticated]);
+
+  const addToWishList = async (item) => {
     const isItemInWishList = wishListItems.find((wishListItem) => wishListItem.id === item.id);
 
+
     if (!isItemInWishList) {
-      setWishListItems([...wishListItems, { ...item }]);
+		setWishListItems([...wishListItems, { ...item }]);
+		if (isAuthenticated) {
+			await fetch(`/add-wishlist`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+				body: JSON.stringify(wishListItems),
+			});
+		}
     }
   };
 
-  const removeItemFromWishList = (item) => {
-    const itemToBeRemoved = wishListItems.find((wishListItem) => item.id === wishListItem.id);
+  const removeItemFromWishList = async (item) => {
+	const itemToBeRemoved = wishListItems.find((wishListItem) => item.id === wishListItem.id);
 
     if (itemToBeRemoved) {
+		if (isAuthenticated) {
+			await fetch(`/remove-item-wishlist/${itemToBeRemoved.id}`,{
+				method: 'DELETE',
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+			});
+		}
         setWishListItems(wishListItems.filter((wishListItem) => item.id !== wishListItem.id));
     }
   };
 
-  const clearWishList = () => {
+  const clearWishList = async () => {
+    if (isAuthenticated) {
+		await fetch('/clear-wishlist', {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('token')}`,
+			},
+	});
+    }
     setWishListItems([]);
   };
 
